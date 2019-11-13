bc4c940750403b97514f8ba4f1ff31461a4aaa7e
KitsuneNoctus <theceintiant@gmail.com>
2019-11-12 23:54:44 -0800

    Adding some class and initializing the sushi and chopsticks.

--- SushiNeko/GameScene.swift
+++ SushiNeko/GameScene.swift
@@ -13,5 +13,14 @@ enum Side{
 }
 
 class GameScene: SKScene {
-
+    /* Game objects */
+    var sushiBasePiece: SushiPiece!
+    
+    override func didMove(to view: SKView) {
+        super.didMove(to: view)
+        /* Connect game objects */
+        sushiBasePiece = childNode(withName: "sushiBasePiece") as! SushiPiece
+        /* Setup chopstick connections */
+        sushiBasePiece.connectChopsticks()
+    }
 }
