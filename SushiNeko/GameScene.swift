a78b91614e3246b6047cd84730291803fca96693
KitsuneNoctus <theceintiant@gmail.com>
2019-11-13 00:07:40 -0800

    Adding main game objects.

--- SushiNeko/GameScene.swift
+++ SushiNeko/GameScene.swift
@@ -15,12 +15,16 @@ enum Side{
 class GameScene: SKScene {
     /* Game objects */
     var sushiBasePiece: SushiPiece!
+    /* Cat Character*/
+    var character: Character!
     
     override func didMove(to view: SKView) {
         super.didMove(to: view)
         /* Connect game objects */
         sushiBasePiece = childNode(withName: "sushiBasePiece") as! SushiPiece
+        character = childNode(withName: "character") as! Character
         /* Setup chopstick connections */
         sushiBasePiece.connectChopsticks()
+        
     }
 }
